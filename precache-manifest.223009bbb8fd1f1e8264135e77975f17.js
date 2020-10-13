f117df8051da223413a01d2765133e825e329c19
Jun Prawita <jprawita@gmail.com>
2020-10-13 15:57:11 +0800

    initial commit

---
+++ precache-manifest.223009bbb8fd1f1e8264135e77975f17.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "998e6e7b21d98edd0258",
+    "url": "/vue-projects/css/about.3eb9bb9f.css"
+  },
+  {
+    "revision": "ab84fa82e32483ff12f2",
+    "url": "/vue-projects/css/app.baa7402a.css"
+  },
+  {
+    "revision": "fa74bfcaab1ffa751a54",
+    "url": "/vue-projects/css/chunk-vendors.17997d7a.css"
+  },
+  {
+    "revision": "5a1cf5b9c4f4cc3111acbe5925fe8fc1",
+    "url": "/vue-projects/img/ArtPurestreamE1.5a1cf5b9.png"
+  },
+  {
+    "revision": "ee6223115cf2dab48b197564e01e0aa0",
+    "url": "/vue-projects/img/Untitled-4.ee622311.jpg"
+  },
+  {
+    "revision": "e40d2d78b54f4b95c8107773e7410eea",
+    "url": "/vue-projects/img/arknight.e40d2d78.jpg"
+  },
+  {
+    "revision": "ce3d82b6185e0bbf06e6d9a38ae52b9e",
+    "url": "/vue-projects/img/bg_gamesocial_brawlstars.ce3d82b6.jpg"
+  },
+  {
+    "revision": "cbb688c4aba6ee1131b6c4865e783bcd",
+    "url": "/vue-projects/img/brawl-stars-text.cbb688c4.png"
+  },
+  {
+    "revision": "cfd2ad4df4afa469f3fdc16672a5902d",
+    "url": "/vue-projects/img/char-crow-modif.cfd2ad4d.png"
+  },
+  {
+    "revision": "f7b21ac84e12075c150edab6fd107df2",
+    "url": "/vue-projects/img/char-penny-modif.f7b21ac8.png"
+  },
+  {
+    "revision": "06783a1e29b849b5e57b2a527e7ac6dc",
+    "url": "/vue-projects/img/char-shelly-modif.06783a1e.png"
+  },
+  {
+    "revision": "1e14f79e45241d5307a66919ae24e586",
+    "url": "/vue-projects/img/class_caster.1e14f79e.png"
+  },
+  {
+    "revision": "f11113be6da3c0e0f353d023afb9148b",
+    "url": "/vue-projects/img/class_defender.f11113be.png"
+  },
+  {
+    "revision": "81bf40bfd498f7d959379c79c3a4381d",
+    "url": "/vue-projects/img/class_guard.81bf40bf.png"
+  },
+  {
+    "revision": "4fee58bdb7c8e8907f6494dca7ce5680",
+    "url": "/vue-projects/img/class_medic.4fee58bd.png"
+  },
+  {
+    "revision": "921c7b0b1f26844eb91d75c118221262",
+    "url": "/vue-projects/img/class_medic_dark.921c7b0b.png"
+  },
+  {
+    "revision": "b4e4c8c1d53ebc7498e923a1027cdc76",
+    "url": "/vue-projects/img/class_sniper.b4e4c8c1.png"
+  },
+  {
+    "revision": "f6065869e9eb337ccf30549d73995307",
+    "url": "/vue-projects/img/class_vanguard.f6065869.png"
+  },
+  {
+    "revision": "e8fc66875cf3dc483c6912c88efecdd3",
+    "url": "/vue-projects/img/default-image.e8fc6687.jpg"
+  },
+  {
+    "revision": "4c38f9413597fce4a923b7c1addad67f",
+    "url": "/vue-projects/img/game-mode-1.4c38f941.png"
+  },
+  {
+    "revision": "f65a8d1a4c77511f1f9d4ab1573f8e28",
+    "url": "/vue-projects/img/game-mode-2.f65a8d1a.png"
+  },
+  {
+    "revision": "935c9343491cbcf99880a169cfbf62f4",
+    "url": "/vue-projects/img/game-mode-3.935c9343.png"
+  },
+  {
+    "revision": "b7fd68c319d486782b9399a789aa128b",
+    "url": "/vue-projects/img/game-mode-4.b7fd68c3.png"
+  },
+  {
+    "revision": "09dff00cd8d9169950d292fedb9da8a2",
+    "url": "/vue-projects/img/game-mode-5.09dff00c.png"
+  },
+  {
+    "revision": "96d5bceeb5386b3468030e164fbab0e8",
+    "url": "/vue-projects/img/game-mode-6.96d5bcee.png"
+  },
+  {
+    "revision": "89965f300fcbaf13522ceed42662df87",
+    "url": "/vue-projects/img/game-mode-7 (2).89965f30.png"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/vue-projects/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "91a97aa289eca45543b6de0ce6d6d443",
+    "url": "/vue-projects/img/mockup-game-brawstart-1.91a97aa2.png"
+  },
+  {
+    "revision": "c814eee30921b447afca52cf67b1ffbc",
+    "url": "/vue-projects/img/season-2.c814eee3.jpg"
+  },
+  {
+    "revision": "24b5ef39598b1d2f758ff8ca9d633ea4",
+    "url": "/vue-projects/img/section-hero-2.24b5ef39.jpg"
+  },
+  {
+    "revision": "63a8555ecd20f31df0efb270d13ee725",
+    "url": "/vue-projects/img/undraw_maker_launch_crhe.63a8555e.svg"
+  },
+  {
+    "revision": "6520f896275ac96abb7e1b1b8b8526c0",
+    "url": "/vue-projects/img/undraw_pair_programming_njlp.6520f896.svg"
+  },
+  {
+    "revision": "cc51a96e70e3cec64fd70c9fe09e71b1",
+    "url": "/vue-projects/img/undraw_progressive_app_m9ms.cc51a96e.svg"
+  },
+  {
+    "revision": "bb50683831d77284d617c3fe809926c0",
+    "url": "/vue-projects/index.html"
+  },
+  {
+    "revision": "998e6e7b21d98edd0258",
+    "url": "/vue-projects/js/about.051e24f4.js"
+  },
+  {
+    "revision": "ab84fa82e32483ff12f2",
+    "url": "/vue-projects/js/app.d429c207.js"
+  },
+  {
+    "revision": "fa74bfcaab1ffa751a54",
+    "url": "/vue-projects/js/chunk-vendors.67a539fd.js"
+  },
+  {
+    "revision": "1bd08574c165d2f9634a8da932f60637",
+    "url": "/vue-projects/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-projects/robots.txt"
+  }
+]);
